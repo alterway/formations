27704490e792ac0acebcec10117093db7c51cd02
ArchiFleKs <lefevre.kevin@gmail.com>
2016-03-14 16:19:21 +0100

    update build script

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ git clone https://github.com/hakimel/reveal.js.git $COURS_DIR/revealjs
 mkdir $COURS_DIR/output-html
 
 while IFS=: read cours modules; do
-    for module in $(echo $modules); do
+    for module in $modules; do
         cat $COURS_DIR/$module >> $COURS_DIR/slide-$cours
     done
     pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=.
