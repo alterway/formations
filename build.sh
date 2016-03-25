4c24be1794db01c91a7cbef4b69dbe4e101b8a1f
rguichard <rguichard@vsense.fr>
2016-03-25 16:27:14 +0100

    allow build with local css/js files or remote ones. Git clone of the revealjs repository is no longer needed

--- build.sh
+++ build.sh
@@ -1,21 +1,32 @@
 #!/bin/bash -xe
 
+#############
+#
+# If you want to build slides in a local environment, you should use the parameter "./build.sh offline".
+# In this way, html files will not fetch their css/js files on Internet.
+# You'll need to have a clone of the github repository in order to have css, js and images files
+#
+# If this build file is used by Travis CI, you can use it as it is.
+############
+
 COURS_DIR=cours
 IMG_DIR=images
 LIST=cours/list.md
+mode=$1
 
-git clone https://github.com/hakimel/reveal.js.git $COURS_DIR/revealjs
-mkdir $COURS_DIR/output-html
+mkdir -p $COURS_DIR/output-html/revealjs/css/theme
 
 while IFS=: read cours modules; do
     for module in $modules; do
         cat $COURS_DIR/$module >> $COURS_DIR/slide-$cours
     done
-    pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. -V slideNumber="true"
+    if [[ $mode == "offline" ]]; then
+      pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. -V slideNumber="true"
+    else
+      pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url="http://formation.osones.com/revealjs" -V slideNumber="true"
+    fi
+
     rm -f $COURS_DIR/slide-$cours
 done < $LIST
 
-cp  $COURS_DIR/output-html/*.html $COURS_DIR/revealjs/
-cp -r $IMG_DIR $COURS_DIR/revealjs/
-cp $COURS_DIR/styles/osones.css $COURS_DIR/revealjs/css/theme/osones.css
-cp -r $COURS_DIR/revealjs/css $COURS_DIR/output-html/
+cp $COURS_DIR/styles/osones.css $COURS_DIR/output-html/revealjs/css/theme/osones.css
