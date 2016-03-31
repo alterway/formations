2aebbd76cbb6bed8277d3c5c30956fcf64ece6b1
rguichard <rguichard@vsense.fr>
2016-03-31 23:33:49 +0200

    replace ## by ### just before html generation in order to handle three levels of sections

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 #
 # If you want to build slides in a local environment, you should use the parameter "./build.sh offline".
 # In this way, html files will not fetch their css/js files on Internet.
-# You'll need to have a clone of the github repository in order to have css, js and images files
+# You'll need to have a clone of the github repository in order to have css, js and images files.
 #
-# If this build file is used by Travis CI, you can use it as it is.
+# If this build file is used by Travis CI, you can use it as it is, with no parameter.
 ############
 
 COURS_DIR=cours
@@ -20,6 +20,8 @@ while IFS=: read cours modules; do
     for module in $modules; do
         cat $COURS_DIR/$module >> $COURS_DIR/slide-$cours
     done
+    sed 's/^## /### /' $COURS_DIR/slide-$cours > tmp_slide-$cours
+    mv tmp_slide-$cours $COURS_DIR/slide-$cours
     if [[ $mode == "offline" ]]; then
       pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. -V slideNumber="true"
     else
