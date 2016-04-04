33a75f84ee21831a2899372c6ab521a50ddcaa9b
rguichard <rguichard@vsense.fr>
2016-04-04 13:46:29 +0200

    rewriting from scratch use function for html and pdf build docker cmd are run directly from this script

--- build.sh
+++ build.sh
@@ -1,34 +1,53 @@
 #!/bin/bash -xe
 
-#############
-#
-# If you want to build slides in a local environment, you should use the parameter "./build.sh offline".
-# In this way, html files will not fetch their css/js files on Internet.
-# You'll need to have a clone of the github repository in order to have css, js and images files.
-#
-# If this build file is used by Travis CI, you can use it as it is, with no parameter.
-############
-
 COURS_DIR=cours
 IMG_DIR=images
 LIST=cours/list.md
-mode=$1
+#mode=$1
+EXT=$2
+COURSE=$1
+#urlRevealjs=$3
 
-mkdir -p $COURS_DIR/output-html/revealjs/css/theme
+function build-html {
+  mkdir -p output-html/revealjs/css/theme
+  cp $COURS_DIR/styles/osones.css output-html/revealjs/css/theme/osones.css
 
-while IFS=: read cours modules; do
+#  if [[ $1 != "" ]]; then
+#    urlRevealjs=$1
+#  fi
+  while IFS=: read cours modules; do
     for module in $modules; do
-        cat $COURS_DIR/$module >> $COURS_DIR/slide-$cours
+      cat $COURS_DIR/$module >> $COURS_DIR/slide-$cours
     done
+
+    # Header2 are only usefull for beamer, they need to be replace with Header3 for revealjs interpretation
     sed 's/^## /### /' $COURS_DIR/slide-$cours > tmp_slide-$cours
     mv tmp_slide-$cours $COURS_DIR/slide-$cours
-    if [[ $mode == "offline" ]]; then
-      pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. -V slideNumber="true"
-    else
-      pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url="http://formation.osones.com/revealjs" -V slideNumber="true"
-    fi
 
+    docker run -v $PWD:/formations osones/revealjs-builder:stable --standalone --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=http://formation.osones.com/revealjs -V slideNumber=true -o /formations/output-html/$cours.html /formations/$COURS_DIR/slide-$cours
     rm -f $COURS_DIR/slide-$cours
-done < $LIST
+  done < $LIST
+}
 
-cp $COURS_DIR/styles/osones.css $COURS_DIR/output-html/revealjs/css/theme/osones.css
+function build-pdf {
+  mkdir -p output-pdf
+  for cours in $(cut -d: -f1 $LIST); do
+    docker run -v $PWD/output-pdf:/output -v $PWD/output-html/"$cours".html:/index.html -v $PWD/images:/images osones/wkhtmltopdf:stable -O landscape -s A5 -T 0 file:///index.html\?print-pdf /output/"$cours".pdf
+  done
+}
+
+case $EXT in
+  html)
+    build-html
+    ;;
+  pdf)
+    build-html
+    build-pdf
+    ;;
+  '')
+    build-html
+    build-pdf
+    ;;
+esac
+
+
