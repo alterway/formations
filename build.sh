9c678f9d308b82302bdb5442e3cd12947af2828b
rguichard <rguichard@vsense.fr>
2016-05-10 15:36:33 +0200

    fix problem with building specific course

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ LIST=cours.list
 TITLE=""
 DATE=""
 
-function build-html {
+build-html() {
   mkdir -p output-html/revealjs/css/theme
   mkdir -p output-html/images
 
@@ -28,13 +28,8 @@ function build-html {
     rm -f $COURS_DIR/slide-$cours
   done < $LIST
 }
-function build-pdf {
+build-pdf() {
   mkdir -p output-pdf
-#  if [[ $3 != "" ]]; then
-#      COURSE=$3
-#      grep $COURSE $LIST > cours.list.tmp
-#      LIST=cours.list.tmp
-#  fi
   for cours in $(cut -d$ -f1 $LIST); do
     docker run -v $PWD/output-pdf:/output -v $PWD/output-html/"$cours".html:/index.html -v $PWD/images:/images osones/wkhtmltopdf:stable -O landscape -s A5 -T 0 file:///index.html\?print-pdf /output/"$cours".pdf
   done
@@ -44,11 +39,13 @@ display_help() {
     cat <<EOF
 USAGE : $0 options
 
--o output           Type of output you desire (html or pdf), if not specified all outputs are built
+-o output           Type of output you desire (html, pdf or all), if not specified all outputs are built
 -t theme            Theme to use
+                    (default : osones)
 -u revealjsURL      RevealJS URL that need to be use. If you build formation supports on local environment
                     you should use "." and git clone http://github.com/hakimel/reveal.js and put your index.html into the repository clone.
                     This option is also necessary even if you only want PDF output
+                    (default : http://formations.osones.com/revealjs)
 -c course           Course to build, if not specified all courses are built
 
 EOF
@@ -66,6 +63,7 @@ while getopts ":o:t:u:c:h" OPT; do
         u) REVEALJSURL="$OPTARG";;
         ?)
             echo "Invalid option: -$OPTARG" >&2
+            display_help
             exit 1
             ;;
     esac
@@ -75,7 +73,7 @@ done
 if [[ $THEME == "" ]]; then
   THEME="osones"
 else
-  ls $COURS_DIR/styles/"$THEME".css 2> /dev/null
+  ls $COURS_DIR/styles/"$THEME".css &> /dev/null
   [ $? -eq 2 ] && echo "Theme $THEME doesn't exist" && exit 1
 fi
 
@@ -90,9 +88,9 @@ if [[ ! $OUTPUT =~ html|pdf|all ]]; then
     display_help
     exit 1
 elif [[ $OUTPUT == "html" ]]; then
-    build-html $REVEALJSURL $THEME $COURSE
+    build-html
 elif [[ $OUTPUT == "pdf" || $OUTPUT == "all" ]]; then
-    build-html $REVEALJSURL $THEME $COURSE
+    build-html
     build-pdf
 fi
 rm -f cours.list.tmp
